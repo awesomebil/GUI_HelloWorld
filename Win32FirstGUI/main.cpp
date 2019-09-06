adb1681630c3143a8ac87e7c9dc11459b99d30c6
Bilal Suleman <bilsuleman@gmail.com>
2019-09-06 18:55:23 -0500

    Added buttons and completed majority of GUI tutorial on controls.

--- Win32FirstGUI/main.cpp
+++ Win32FirstGUI/main.cpp
@@ -3,11 +3,15 @@
 #define FILE_MENU_NEW 1
 #define FILE_MENU_OPEN 2
 #define FILE_MENU_EXIT 3
+#define CHANGE_TITLE 4
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 void AddMenus(HWND);
+void AddControls(HWND);
 
 HMENU hMenu;
+HWND hEdit;
+HWND hButton;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
 {
@@ -48,11 +52,17 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 			break;
 		case FILE_MENU_NEW:
 			MessageBeep(MB_ICONINFORMATION);
+		case CHANGE_TITLE:
+			wchar_t text[100];
+			GetWindowTextW(hEdit, text, 100);
+			SetWindowTextW(hWnd, text);
+			break;
 		}
 
 		break;
 	case WM_CREATE:
 		AddMenus(hWnd);
+		AddControls(hWnd);
 		return 0;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -69,7 +79,7 @@ void AddMenus(HWND hWnd)
 	HMENU hFileMenu = CreateMenu();
 	HMENU hSubMenu = CreateMenu();
 
-	AppendMenu(hSubMenu, MF_STRING, NULL, "SubMenu Item");
+	AppendMenu(hSubMenu, MF_STRING, NULL, "Change Title");
 
 	AppendMenu(hFileMenu, MF_STRING, FILE_MENU_NEW, "New");
 	AppendMenu(hFileMenu, MF_POPUP, (UINT_PTR) hSubMenu, "Open SubMenu");
@@ -80,4 +90,14 @@ void AddMenus(HWND hWnd)
 	AppendMenu(hMenu, MF_STRING, NULL, "Help");
 
 	SetMenu(hWnd, hMenu);
+}
+
+void AddControls(HWND hWnd)
+{
+	CreateWindowW(L"static", L"Enter Text Here: ", WS_VISIBLE | WS_CHILD | WS_BORDER | SS_CENTER, 200, 50, 100, 50, hWnd, 
+				NULL, NULL, NULL);
+	hEdit = CreateWindowW(L"edit", L"...", WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
+				200, 100, 100, 50, hWnd, NULL, NULL, NULL);
+	hButton = CreateWindowW(L"button", L"Change Title", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON | BS_CENTER, 200, 200, 
+				100, 50, hWnd, (HMENU)CHANGE_TITLE, NULL, NULL);
 }
